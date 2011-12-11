2f80a4989e5e78d076b36373445c5700c11eed3a
khs <khs@meh.localdomain>
2011-12-11 11:45:00 +0100

    adde fibonacci test

---
+++ interpreter/tests/fib.hs
@@ -0,0 +1,10 @@
+
+main = do 
+    let n = 10
+    putStrLn $ "The 10. fibonacci number is: " ++ (show $ fib n)
+
+fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
+
--- interpreter/tests/helloworld.hs
+++ interpreter/tests/helloworld.hs
@@ -1,3 +1,3 @@
 
-main = putStrLn "hello world!"
+main = putStrLn "Hello, world!"
 
