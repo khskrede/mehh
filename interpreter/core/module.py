cf8b2c48375e90ae2b011ab6ad29285f68394138
khs <khs@meh.localdomain>
2012-03-14 19:12:58 +0100

    meh

---
+++ interpreter/core/module.py
@@ -0,0 +1,11 @@
+
+import haskell.haskell
+
+class module():
+    name = "" 
+    vdefg = {}
+    tdefg = {}
+    qvars = {}
+    def __init__(self):
+        self.vdefg = {}
+        self.tdefg = {}
