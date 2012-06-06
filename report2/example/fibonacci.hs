feeb7a29e5b46cc0036c56e1e22474199bec2e44
U-wee\khs <khs@wee.(none)>
2012-06-06 13:45:52 +0200

    final touches

---
+++ report2/example/fibonacci.hs
@@ -0,0 +1,11 @@
+module Main where
+
+main = do 
+    let n = 10
+    putStrLn $ "The 10. fibonacci number is: " ++ (show $ fib n)
+
+fib :: Int -> Int
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
+
